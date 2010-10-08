d1560e01511079ef5426df7585b203c30463794f
Kien Trinh <kientt86@gmail.com>
2010-10-08 18:14:09 +0700

    Push a very fist version

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup,Extension
+setup(name='hdfs',
+        version='1.0',
+        description='Python Bindings for HDFS',
+        author='Kien Trinh',
+        author_email='kientt86@gmail.com',
+        url='git@github.com:kientt86/libhdfs.git',
+        ext_modules=[Extension(name='hdfs', sources=['hdfs.c'],  libraries=['hdfs','jvm'])])
